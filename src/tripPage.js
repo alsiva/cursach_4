a9db4fb602dc55b16dd45d54ecbc55875bcfea7e
alexi <alexivanovrus@gmail.com>
2022-12-23 17:01:27 +0300

    Added submition state

--- src/tripPage.js
+++ src/tripPage.js
@@ -1,42 +1,56 @@
 import React, {useState, useEffect } from 'react'
 import {CircularProgress} from "@mui/material";
 
-// denied, confirmed, applied, idle
+function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms)
+    })
+}
 
 
 export default function TripPage({trip, userInfo, back}) {
-    const [applicationStatus, setApplicationStatus] = useState(null)
-    useEffect(() => {
-        setTimeout(() => {
-            setApplicationStatus({
-                state: 'idle'
-            })
-        }, 1000)
-    }, [])
-
     return (
         <div>
             <button onClick={back}>back to trip list</button>
             <h3>{trip.title}</h3>
             <p>Starts on {trip.startDate}, ends on {trip.endDate}</p>
             <h4>Application status</h4>
-            <ApplicationStatus status={applicationStatus}/>
+            <ApplicationStatus />
         </div>
     )
 }
 
-function ApplicationStatus(status) {
+function ApplicationStatus() {
+    const [status, setState] = useState(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setState({
+                state: 'idle'
+            })
+        }, 1000)
+    }, [])
+
+    async function submitApplication(text) {
+        setState(null)
+        await delay(500)
+        setState({
+            state: 'applied'
+        })
+
+        await(5000)
+        setState({
+            state: Math.random() > 0.5 ? 'confirmed' : 'denied'
+        })
+    }
+
     if (status === null) {
         return <CircularProgress/>
     }
 
     if (status.state === 'idle') {
         return (
-            <div>
-                <h4>Submit your letter</h4>
-                <textarea></textarea>
-                <button>Submit</button>
-            </div>
+            <ApplicationSubmit onSubmit={submitApplication} />
         )
     }
 
@@ -57,4 +71,17 @@ function ApplicationStatus(status) {
             <p>Congratulation! You has been selected for this trip!</p>
         )
     }
+}
+
+
+function ApplicationSubmit({onSubmit}) {
+    const [text, setText] = useState("")
+
+    return (
+        <div>
+            <h4>Submit your letter</h4>
+            <textarea value={text} onChange={e => setText(e.currentTarget.value)}/>
+            <button onClick={() => onSubmit(text)}>Submit</button>
+        </div>
+    )
 }
