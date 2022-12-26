076d9fd3b04390b4b08b7e5a4f8ad5c6e1a652d5
alexi <alexivanovrus@gmail.com>
2022-12-27 02:27:39 +0300

    Updated react rendering

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './app';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 import {LocalizationProvider} from "@mui/x-date-pickers";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
     <React.StrictMode>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <App/>
         </LocalizationProvider>
     </React.StrictMode>,
-    document.getElementById('root')
 );
