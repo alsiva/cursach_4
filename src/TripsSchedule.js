d4a3d8e34d23b37617b352163c6f9248dc7146b2
alexi <alexivanovrus@gmail.com>
2023-01-12 08:47:06 +0300

    TripSchedule works

--- src/TripsSchedule.js
+++ src/TripsSchedule.js
@@ -4,35 +4,38 @@ import {Box, Button, CircularProgress, Divider, Stack, TextField} from "@mui/mat
 import dayjs from "dayjs";
 import {DateTimePicker} from "@mui/x-date-pickers";
 
-async function getSchedule(tripId) {
-    await delay(500)
 
-    const response = await fetch(`/api/trips/${tripId}/schedule`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-
-    console.log('Response')
-    console.log(response)
-
-    if (response.status === 404) {
-        return []
-    }
-
-    return (await response.json()).map(schedule => (
-        {
-            id: schedule.id,
-            startTime: schedule.startTime,
-            endTime: schedule.endTime,
-            description: schedule.description
-        }
-    ))
-}
 
 export default function TripSchedule({trip, userInfo, back}) {
     const [schedules, setSchedules] = useState(null)
+
+    const BACK_FORMAT = 'YYYY-MM-DDTHH:mm'
+
+    async function getSchedule(tripId) {
+        await delay(500)
+
+        const response = await fetch(`/api/trips/${tripId}/schedule`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+
+        console.log('Response')
+        console.log(response)
+
+        if (response.status === 404) {
+            return []
+        }
+
+        return (await response.json()).map(schedule => (
+            {
+                start: dayjs(schedule.start).format(BACK_FORMAT),
+                end: dayjs(schedule.end).format(BACK_FORMAT),
+                description: schedule.description
+            }
+        ))
+    }
 
     useEffect(() => {
         getSchedule(trip.id).then(schedule => setSchedules(schedule))
@@ -48,16 +51,16 @@ export default function TripSchedule({trip, userInfo, back}) {
             <Box sx={{ p: 2, border: '1px dashed grey' }}>
                 <ul>
                     {schedules.map(schedule => (
-                        <li key={schedule.id}>
+                        <li key={schedule.start}>
                             <Stack divider={<Divider orientation="vertical" flexItem/>}
                                    direction={"row"}
                                    spacing={1}
                                    alignItems={"center"}
                             >
                                 {userInfo.right === 'organizer' &&
-                                    <Button onClick={() => deleteSchedule(schedule.id)}>Delete</Button>
+                                    <Button onClick={() => deleteSchedule(trip.id, schedule.start, schedule.end)}>Delete</Button>
                                 }
-                                <h3>{schedule.startTime} - {schedule.endTime}:</h3>
+                                <h3>{schedule.start} - {schedule.end}:</h3>
                                 <h3>{schedule.description}</h3>
                             </Stack>
                         </li>
@@ -74,27 +77,43 @@ export default function TripSchedule({trip, userInfo, back}) {
         </div>
     )
 
-    async function deleteSchedule(toRemoveId) {
-        const response = await fetch('/api/schedule/' + toRemoveId, {
+
+
+    async function deleteSchedule(tripId, startTime, endTime) {
+
+        startTime = dayjs(startTime).format(BACK_FORMAT)
+        endTime = dayjs(endTime).format(BACK_FORMAT)
+
+        await fetch(`/api/trips/${tripId}/schedule?startTime=${startTime}&endTime=${endTime}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
             }
         })
+        console.log('Debug data')
+        console.log(schedules[0])
+        console.log(startTime)
 
-        await response.json()
 
-        setSchedules(prev => prev.filter(schedule => schedule.id !== toRemoveId))
+        setSchedules(prev => prev.filter(schedule => schedule.start !== startTime))
     }
+
+
+
 
     async function addSchedule(startTime, endTime, description) {
 
 
-        const BACK_FORMAT = 'YYYY-MM-DDTHH:mm'
-        startTime = dayjs(startTime).format(BACK_FORMAT)
-        endTime = dayjs(endTime).format(BACK_FORMAT)
+
         console.log('startTime')
         console.log(startTime)
+
+        startTime = dayjs(startTime).format(BACK_FORMAT)
+        endTime = dayjs(endTime).format(BACK_FORMAT)
+
+        console.log('startTime')
+        console.log(startTime)
+
 
         const response = await fetch(`/api/trips/${trip.id}/schedule?startTime=${startTime}&endTime=${endTime}&description=${description}`, {
             method: 'POST',
@@ -104,14 +123,12 @@ export default function TripSchedule({trip, userInfo, back}) {
         })
 
         const json = await response.json()
-        console.log('json')
-        console.log(json)
+
 
         setSchedules(prev => [
             ...prev, {
-                id: json.id,
-                startTime: json.startTime,
-                endTime: json.endTime,
+                start: dayjs(json.start).format(BACK_FORMAT),
+                end: dayjs(json.end).format(BACK_FORMAT),
                 description: json.description
             }
         ])
@@ -119,48 +136,49 @@ export default function TripSchedule({trip, userInfo, back}) {
         console.log('schedules')
         console.log(schedules)
     }
+
+    function CreateSchedule({addSchedule}) {
+        const [startTime, setStartTime] = useState(dayjs())
+        const [endTime, setEndTime] = useState(dayjs())
+        const [description, setDescription] = useState("")
+
+
+        return (
+            <div>
+                <Stack maxWidth={400} spacing={1}>
+                    <DateTimePicker
+                        label="StartTime"
+                        inputFormat={BACK_FORMAT}
+                        value={startTime}
+                        onChange={(dateTime) => {
+                            setStartTime(dateTime)
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                    <DateTimePicker
+                        label="EndTime"
+                        inputFormat={BACK_FORMAT}
+                        value={endTime}
+                        onChange={(dateTime) => {
+                            setEndTime(dateTime)
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                    <textarea
+                        value={description}
+                        onChange={e => setDescription(e.currentTarget.value)}
+                    />
+                    <Button
+                        variant="outlined"
+                        onClick={() => addSchedule(startTime.format(BACK_FORMAT), endTime.format(BACK_FORMAT), description)}
+                    >
+                        Add Schedule
+                    </Button>
+                </Stack>
+            </div>
+        )
+    }
 }
 
-const TIMESTAMP_FORMAT = "DD/MM/YYYY HH:mm:ss"
-
-function CreateSchedule({addSchedule}) {
-    const [startTime, setStartTime] = useState(dayjs())
-    const [endTime, setEndTime] = useState(dayjs())
-    const [description, setDescription] = useState("")
 
 
-    return (
-        <div>
-            <Stack maxWidth={400} spacing={1}>
-                <DateTimePicker
-                    label="StartTime"
-                    inputFormat={TIMESTAMP_FORMAT}
-                    value={startTime}
-                    onChange={(dateTime) => {
-                        setStartTime(dateTime)
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-                <DateTimePicker
-                    label="EndTime"
-                    inputFormat={TIMESTAMP_FORMAT}
-                    value={endTime}
-                    onChange={(dateTime) => {
-                        setEndTime(dateTime)
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-                <textarea
-                    value={description}
-                    onChange={e => setDescription(e.currentTarget.value)}
-                />
-                <Button
-                    variant="outlined"
-                    onClick={() => addSchedule(startTime.format(TIMESTAMP_FORMAT), endTime.format(TIMESTAMP_FORMAT), description)}
-                >
-                    Add Schedule
-                </Button>
-            </Stack>
-        </div>
-    )
-}
